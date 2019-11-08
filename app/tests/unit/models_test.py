588e314b7a7a4164222ae5a89e0251b6e975b2fb
gabefoley <gabriel.foley@uqconnect.edu.au>
2019-11-08 11:59:19 +1000

    Added coverage

--- app/tests/unit/models_test.py
+++ app/tests/unit/models_test.py
@@ -1,4 +1,5 @@
 from app.models import Artist
+import app.lastfm
 
 def test_new_artist():
     """
@@ -9,3 +10,15 @@ def test_new_artist():
     artist = Artist('2', 'Black Flag', 55, 'www.url')
     assert artist.name == 'Black Flag'
     assert artist.popularity == 55
+
+
+
+def test_artist_list_size():
+    """
+    Check that getting the artist list from Last.fm returns the correct size
+    :return:
+    """
+
+    artist_list = app.lastfm.get_artist_list(10)
+
+    assert len(artist_list) == 10
